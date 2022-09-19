7c3fe198843d0705ad04f79e9ceb95518a2f0f07
Avocablock09 <avocablock@gmail.com>
2022-09-19 20:34:58 +0800

    login_view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,10 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
     <style>
-        @media screen and (min-height: 600px) {
-            body {
-                min-height: 100vh;
-            }
+        html,body{
+            height: 100%;
         }
     </style>
 </head>
